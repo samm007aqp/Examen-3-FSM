9eff6192d2cff2ea0a468cd62e6df1849af7d05b
Sergio Montesinos <smontesinosa@unsa.edu.pe>
2020-08-11 20:11:01 -0500

    Segundo Commint

--- CG.py
+++ CG.py
@@ -9,34 +9,25 @@ image = cv2.imread('tasa2.jpg')
 
 training_image = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 image2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
 training_gray = cv2.cvtColor(training_image, cv2.COLOR_RGB2GRAY)
-
 test_gray = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
 
-
 fx, plots = plt.subplots(1, 2, figsize=(20,10))
-
-plots[0].set_title("Training Image")
+plots[0].set_title("Image 1")
 plots[0].imshow(training_image)
-
-plots[1].set_title("Testing Image")
+plots[1].set_title("Image 2")
 plots[1].imshow(image2)
 
 sift = cv2.xfeatures2d.SIFT_create()
-
-
 train_keypoints, train_descriptor = sift.detectAndCompute(training_gray, None)
-print(len(train_descriptor[3]) ) 
-
+print(len(train_descriptor[3])) 
 test_keypoints, test_descriptor = sift.detectAndCompute(test_gray, None)
-print(test_keypoints[3] ) 
+print(test_keypoints[3]) 
 
 keypoints_without_size = np.copy(training_image)
 keypoints_with_size = np.copy(training_image)
 
 cv2.drawKeypoints(training_image, train_keypoints, keypoints_without_size, color = (0, 255, 0))
-
 cv2.drawKeypoints(training_image, train_keypoints, keypoints_with_size, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 fx, plots = plt.subplots(1, 2, figsize=(20,10))
@@ -55,11 +46,11 @@ print("Number Keypoints image2 ", len(test_keypoints))
 bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck = False)
 matches = bf.match(train_descriptor, test_descriptor)
 matches = sorted(matches, key = lambda x : x.distance)
-result = cv2.drawMatches(training_image, train_keypoints, image2, test_keypoints, matches, test_gray, flags = 2)
+result = cv2.drawMatches(training_image, train_keypoints, image2, test_keypoints, matches, image2, flags = 2)
 
 
 plt.rcParams['figure.figsize'] = [14.0, 7.0]
-plt.title('Best Matching Points')
+plt.title('Full Matching Points')
 plt.imshow(result)
 plt.show()
 
@@ -81,7 +72,7 @@ pts2 = []
 for i,(m,n) in enumerate(matches):
 	if m.distance < 0.8*n.distance:
 		#print(m.distance)
-		good.append(m)
+		good.append([m])
 		pts2.append(test_keypoints[m.trainIdx].pt)
 		pts1.append(train_keypoints[m.queryIdx].pt)
 
@@ -112,14 +103,10 @@ def drawlines(img1,img2,lines,pts1,pts2):
 		img2 = cv2.circle(img2,tuple(pt2),5,color,-1)
 	return img1,img2
 
-
-# Find epilines corresponding to points in right image (second image) and
-# drawing its lines on left image
 lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
 lines1 = lines1.reshape(-1,3)
 img5,img6 = drawlines(img1,img2,lines1,pts1,pts2)
-# Find epilines corresponding to points in left image (first image) and
-# drawing its lines on right image
+
 lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
 lines2 = lines2.reshape(-1,3)
 img3,img4 = drawlines(img2,img1,lines2,pts2,pts1)
@@ -127,15 +114,12 @@ plt.subplot(121),plt.imshow(img5)
 plt.subplot(122),plt.imshow(img3)
 plt.show()
 
-result = cv2.drawMatches(image1, train_keypoints, image2, test_keypoints, matches, test_gray, flags = 2)
+result = cv2.drawMatchesKnn(training_image,train_keypoints, image2,test_keypoints, good, None, flags = 2)
 
 plt.rcParams['figure.figsize'] = [14.0, 7.0]
-plt.title('Best Matching Points')
+plt.title('Knn Maching')
 plt.imshow(result)
 plt.show()
 
 
 
-
-# Print total number of matching points between the training and query images
-print("\nNumber of Matching Keypoints Between The Training and Query Images: ", len(matches))
